245cf2bc0044c81a3e843f8646a85541aa3f6579
williebtc <williebtc@outlook.com>
2021-06-19 19:37:37 +0100

    Added a userModel and referralModel

---
+++ models/ReferralModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const referralSchema = mongoose.Schema(
+  {
+    theReferreeUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    referredUsername: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    isVerified: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    isReferralPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const ReferralModel = mongoose.model("Referral", referralSchema);
+module.exports = ReferralModel;
