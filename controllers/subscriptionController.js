f1827ab320fcf394c6eb5e74c8e0b895c16684e5
Bonaventure <bonarhyme@gmail.com>
2021-08-04 17:02:43 +0100

    Added a check to stop a duplicate subscription

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const FormData = require("form-data");
 const data = new FormData();
-const AsyncHandler = require("express-async-handler");
+const asyncHandler = require("express-async-handler");
 const Subscription = require("../models/subscriptionModel");
 const User = require("../models/userModel");
 const generateExpiryDate = require("../utility/generateExpiryDate");
@@ -24,110 +24,129 @@ const sendPaystackConfig = (req, res) => {
  * @access This a private routes
  */
 
-const confirmPayment = AsyncHandler(async (req, res) => {
-  try {
-    const {
-      message,
-      reference,
-      status,
-      trans,
-      transaction,
-      trxref,
-      amount,
-      plan,
-      duration,
-    } = req.body;
-    const { username, name, email, _id } = req.user;
+const confirmPayment = asyncHandler(async (req, res) => {
+  const {
+    message,
+    reference,
+    status,
+    trans,
+    transaction,
+    trxref,
+    amount,
+    plan,
+    duration,
+  } = req.body;
 
-    const userExists = await User.findById(_id);
-    let updatedUser;
+  if (
+    !message ||
+    !reference ||
+    !status ||
+    !trans ||
+    !transaction ||
+    !trxref ||
+    !amount ||
+    !plan ||
+    !duration
+  ) {
+    res.status(400);
+    throw new Error("Your request appears to be incomplete.");
+  }
+  const { username, name, email, _id } = req.user;
 
-    if (!userExists) {
-      console.log("User not found.");
-      res.status(404);
-      throw new Error("User not found.");
-    }
+  const userExists = await User.findById(_id);
+  let updatedUser;
 
-    var config = {
-      method: "get",
-      url: `https://api.paystack.co/transaction/verify/${reference}`,
-      headers: {
-        Authorization: `Bearer ${process.env.PAYSTACK_ACC_SECRET_KEY}`,
-        ...data.getHeaders(),
-      },
-      data: data,
-    };
+  if (!userExists) {
+    console.log("User not found.");
+    res.status(404);
+    throw new Error("User not found.");
+  }
+  const userHasActiveSub = await User.find({ _id, "activeSub.active": true });
 
-    axios(config)
-      .then(async function (response) {
-        const res = response.data.data;
-        const cus = response.data.data.customer;
-        const newSub = await new Subscription({
-          message,
-          reference,
-          transaction,
-          amount,
-          plan,
-          user: {
-            username,
-            userId: _id,
-            email,
-          },
-          paymentCustomer: {
-            id: cus.id,
-            first_name: cus.first_name,
-            last_name: cus.last_name,
-            email: cus.email,
-            customer_code: cus.customer_code,
-            phone: cus.phone,
-          },
-          authorization: res.authorization,
-          paymentData: {
-            domain: res.domain,
-            status: res.status,
-            reference: res.reference,
-            amount: res.amount / 100,
-            paid_at: res.paid_at,
-            created_at: res.created_at,
-            channel: res.channel,
-            currency: res.currency,
-            ip_address: res.ip_address,
-          },
-          log: res.log,
-        });
+  if (userHasActiveSub) {
+    res.status(400);
+    throw new Error(
+      "You can't subscribe at the moment because you still have an active subscription."
+    );
+  }
 
-        userExists.subCount = userExists.subCount + 1;
-        userExists.activeSub.active = true;
-        userExists.activeSub.createdDate = reference;
-        userExists.activeSub.duration = Number(duration);
-        userExists.activeSub.expiryDate = generateExpiryDate(
-          Number(reference),
-          Number(duration)
-        ).expiryDateNumber;
-        userExists.activeSub.expiryDateLiteral = generateExpiryDate(
-          Number(reference),
-          Number(duration)
-        ).expiryDateLiteral;
-        userExists.activeSub.plan = plan;
-        userExists.activeSub.amount = amount;
-        userExists.activeSub.currency = res.currency;
+  var config = {
+    method: "get",
+    url: `https://api.paystack.co/transaction/verify/${reference}`,
+    headers: {
+      Authorization: `Bearer ${process.env.PAYSTACK_ACC_SECRET_KEY}`,
+      ...data.getHeaders(),
+    },
+    data: data,
+  };
 
-        const subSaved = await newSub.save();
-        updatedUser = await userExists.save();
-      })
-
-      .catch(function (error) {
-        console.log(error);
+  axios(config)
+    .then(async function (response) {
+      const res = response.data.data;
+      const cus = response.data.data.customer;
+      const newSub = await new Subscription({
+        message,
+        reference,
+        transaction,
+        amount,
+        plan,
+        user: {
+          username,
+          userId: _id,
+          email,
+        },
+        paymentCustomer: {
+          id: cus.id,
+          first_name: cus.first_name,
+          last_name: cus.last_name,
+          email: cus.email,
+          customer_code: cus.customer_code,
+          phone: cus.phone,
+        },
+        authorization: res.authorization,
+        paymentData: {
+          domain: res.domain,
+          status: res.status,
+          reference: res.reference,
+          amount: res.amount / 100,
+          paid_at: res.paid_at,
+          created_at: res.created_at,
+          channel: res.channel,
+          currency: res.currency,
+          ip_address: res.ip_address,
+        },
+        log: res.log,
       });
 
-    if (updatedUser) {
-      res.send({ message: "Payment has been successfully confirmed." });
-    }
+      userExists.subCount = userExists.subCount + 1;
+      userExists.activeSub.active = true;
+      userExists.activeSub.createdDate = reference;
+      userExists.activeSub.duration = Number(duration);
+      userExists.activeSub.expiryDate = generateExpiryDate(
+        Number(reference),
+        Number(duration)
+      ).expiryDateNumber;
+      userExists.activeSub.expiryDateLiteral = generateExpiryDate(
+        Number(reference),
+        Number(duration)
+      ).expiryDateLiteral;
+      userExists.activeSub.plan = plan;
+      userExists.activeSub.amount = amount;
+      userExists.activeSub.currency = res.currency;
 
-    res.end();
-  } catch (error) {
-    console.log(error);
+      const subSaved = await newSub.save();
+      updatedUser = await userExists.save();
+    })
+
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  if (updatedUser) {
+    res.send({ message: "Payment has been successfully confirmed." });
   }
+
+  res.end();
 });
 
 module.exports = {
