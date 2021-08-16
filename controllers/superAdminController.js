65ced861bfe0e7dd98e97ccd84b03325812f37ca
Bonaventure <bonarhyme@gmail.com>
2021-08-16 20:06:12 +0100

    Added feature to remove user as an admin

--- controllers/superAdminController.js
+++ controllers/superAdminController.js
@@ -9,7 +9,7 @@ const User = require("../models/userModel");
  */
 
 const getRegularUsers = asyncHandler(async (req, res) => {
-  const pageSize = 20;
+  const pageSize = 10;
   const page = Number(req.query.pageNumber);
 
   const count = await User.countDocuments({
@@ -17,6 +17,37 @@ const getRegularUsers = asyncHandler(async (req, res) => {
     isAdmin: false,
   });
   const allUsers = await User.find({ isSuperAdmin: false, isAdmin: false })
+    .select(["-password", "-token", "-isSuperAdmin"])
+    .limit(pageSize)
+    .skip(pageSize * (page - 1))
+    .sort({
+      createdAt: -1,
+    });
+
+  if (allUsers) {
+    res.send({ allUsers, page, pages: Math.ceil(count / pageSize) });
+  } else {
+    res.status(404);
+    throw new Error("No user was found.");
+  }
+});
+
+/**
+ * @description This gets all admin users
+ * @description The routes are GET request of /api/super-admin/users/admin/?pageNumber=theNumber
+ * @description It depends on pageNumber passed as the query
+ * @access This is an super admin only page
+ */
+
+const getAdminUsers = asyncHandler(async (req, res) => {
+  const pageSize = 10;
+  const page = Number(req.query.pageNumber);
+
+  const count = await User.countDocuments({
+    isSuperAdmin: false,
+    isAdmin: true,
+  });
+  const allUsers = await User.find({ isSuperAdmin: false, isAdmin: true })
     .select(["-password", "-token", "-isSuperAdmin"])
     .limit(pageSize)
     .skip(pageSize * (page - 1))
@@ -62,4 +93,5 @@ const makeAdmin = asyncHandler(async (req, res) => {
 module.exports = {
   getRegularUsers,
   makeAdmin,
+  getAdminUsers,
 };
--- routes/superAdminRoutes.js
+++ routes/superAdminRoutes.js
@@ -7,6 +7,7 @@ const superAdmin = require("../middleware/superAdminMiddleware");
 const {
   getRegularUsers,
   makeAdmin,
+  getAdminUsers,
 } = require("../controllers/superAdminController");
 
 // /api/super-admin/users/regular/
@@ -14,5 +15,8 @@ router.get("/users/regular", protect, superAdmin, getRegularUsers);
 
 // /api/super-admin/users/regular/:id
 router.put("/users/regular/:id", protect, superAdmin, makeAdmin);
+
+// /api/super-admin/users/admin/:id
+router.get("/users/admin", protect, superAdmin, getAdminUsers);
 
 module.exports = router;
