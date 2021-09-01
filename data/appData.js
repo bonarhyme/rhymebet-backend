f09a07f45db49f672837ff5f3db365553586cbd5
Bonaventure <bonarhyme@gmail.com>
2021-09-01 21:51:21 +0100

    Added frontend link

--- data/appData.js
+++ data/appData.js
@@ -1,6 +1,6 @@
 const variables = {
-  frontendLink: "http://localhost:3000",
-  // frontendLink: "https://rhymebet.com",
+  // frontendLink: "http://localhost:3000",
+  frontendLink: "https://rhymebet.com",
   // rhymebetLogo: "https:/rhymebet.com/rhymebet-logo-complete.png",
   rhymebetLogo: "Rhymebet",
 };
